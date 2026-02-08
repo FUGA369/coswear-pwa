1babd14bc1cf7af483a23219f0f84f4059d28d9b
FUGA369 <grandcross.ga@gmail.com>
2026-02-08 14:42:57 +0900

    Ensure logo link displays across pages

--- components/TopHeader.jsx
+++ components/TopHeader.jsx
@@ -1,7 +1,9 @@
 export default function TopHeader() {
   return (
-    <header className="site-header">
-      <img src="/icons/hue-logo.svg" alt="HUE" className="site-logo" />
+    <header className="site-header" aria-label="HUE">
+      <a href="/" className="site-logo-link">
+        <img src="/icons/hue-logo.svg" alt="HUE" className="site-logo" />
+      </a>
     </header>
   );
 }
